73643a9cb7752ef7d29c9f5b4e6b7c8526faa09b
learning-dev <ayazm.140895@gmail.com>
2019-10-08 17:41:01 +0530

    Add home url path

--- bank/urls.py
+++ bank/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('', views.get_bank, name='bank-ifsc'),
     path('bank/', views.get_bank, name='bank'),
     path('branches/', views.get_branches, name='branches'),
 ]
