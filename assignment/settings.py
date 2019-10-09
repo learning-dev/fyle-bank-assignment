1ab386fac8fa4b546a082b50e3109c3cc5af6e69
learning-dev <ayazm.140895@gmail.com>
2019-10-09 23:13:58 +0530

    remove secret key from settings file

--- assignment/settings.py
+++ assignment/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'cljofnaid_n#y&ja_e&@ps2wzs$gp9m*ez*i8coo#qbs%8pk%a'
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
